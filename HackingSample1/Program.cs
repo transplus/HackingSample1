458cee96d1eb820171b2a35d383b2102730de0b9
transplus <dev@transpluscorp.com>
2014-10-02 10:06:49 -0400

    Initial creation of the solution.

---
+++ HackingSample1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HackingSample1
+{
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            // Indicates whether the software is licensed.
+            bool licensed;
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            licensed = bool.Parse(ConfigurationManager.AppSettings["licensed"]);
+
+            Application.Run(new Form1());
+        }
+    }
+}
